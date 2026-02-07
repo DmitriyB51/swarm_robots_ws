d7c01ab9ea4b84eaa755f1fcec4217e282b7af4e
DmitriyB51 <d.v.belinskiy51@mail.ru>
2026-02-07 22:05:38 +0500

    added vtol_nav package

---
+++ src/vtol_nav/setup.py
@@ -0,0 +1,30 @@
+from setuptools import find_packages, setup
+
+package_name = 'vtol_nav'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='dmitriyb51',
+    maintainer_email='d.v.belinskiy51@mail.ru',
+    description='TODO: Package description',
+    license='Apache-2.0',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+            'hello_vtol = vtol_nav.hello_vtol:main',
+        ],
+    },
+)
