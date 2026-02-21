bea657be2839928d5f66810a242ad9c04805df96
DmitriyB51 <d.v.belinskiy51@mail.ru>
2026-02-22 01:16:57 +0500

    added client

--- src/vtol_nav/setup.py
+++ src/vtol_nav/setup.py
@@ -27,6 +27,7 @@ setup(
             'hello_vtol = vtol_nav.hello_vtol:main',
             'leader_follower = vtol_nav.leader_follower:main',
             'swarm_navigate_server = vtol_nav.swarm_navigate_server:main',
+            'swarm_navigate_client = vtol_nav.swarm_navigate_client:main',
             
         ],
     },
